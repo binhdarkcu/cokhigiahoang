229a6df65f3c0999b57f48a12f3028ee80590300
Thanh Che <cqthanh.zx@gmail.com>
2018-11-06 00:54:18 +0700

    added sliders

--- wp-content/themes/cokhihoanggia/template-parts/slider.php
+++ wp-content/themes/cokhihoanggia/template-parts/slider.php
@@ -1,69 +1,45 @@
+<?php
+$query = new WP_Query(array(
+    'post_type' => 'slider',
+    'post_status' => 'publish',
+    'orderby' => 'date',
+    'order'   => 'ASC',
+));
+?>
 <div class="mainflex flexslider loading">
-
-            <div class="loading-inn"><i class="fa fa-circle-o-notch fa-spin"></i></div>
-
-
-            <ul class="slides" >
-
-
-
-
-
-                <li>
-
-
-                    <a href="" title="Main contractor for mega projects." >
-
-                        <img width="1600" height="620" src="http://capethemes.com/demo/processing/wp-content/uploads/2015/07/mining-excavator-1736289_1920-1600x620.jpg" class="tranz wp-post-image" alt="" />
-                    </a>
-
-
+    <div class="loading-inn"><i class="fa fa-circle-o-notch fa-spin"></i></div>
+    <ul class="slides" >
+        <?php while ($query->have_posts()): ?>
+            <?php
+            $query->the_post();
+            $post_id = get_the_ID();
+            $post_content = get_the_content();
+            $image_url = wp_extract_urls($post_content);
+            $url_slider = get_post_custom_values('wpcf-url-cua-slider', $post_id);
+            $url_1 = get_post_custom_values('wpcf-url-1', $post_id);
+            $url_2 = get_post_custom_values('wpcf-url-2', $post_id);
+            $url_1_name = get_post_custom_values('wpcf-noi-dung-url-1', $post_id);
+            $url_2_name = get_post_custom_values('wpcf-noi-dung-url-2', $post_id);
+            $subheading = get_post_custom_values('wpcf-tieu-de-phu-cua-slider', $post_id);
+            $slider_description = get_post_custom_values('wpcf-mo-ta-cua-slider', $post_id);
+            ?>
+            <li>
+                <a href="<?php echo $url_slider[0]; ?>" title="<?php echo $subheading[0];?>" >
+                    <img width="1600" height="620" src="<?php echo $image_url[0];?>" class="tranz wp-post-image" alt="" />
+                </a>
+                <?php if($subheading || $slider_description): ?>
                     <div class="flexinside rad tranz content_Left">
-
-                        <h2><a href="" title="Main contractor for mega projects." >Main contractor for mega projects.</a></h2>
-
+                        <h2><a href="" title="<?php echo $subheading[0]; ?>" > <?php echo $subheading[0]; ?> </a></h2>
                         <div class="flexinside-inn">
-
-                            <p>Effective management of mega-projects relies on three key concepts: early planning and organizing, stakeholder communication and project controls integration.</p>
+                            <p> <?php echo $slider_description[0]; ?></p>
                             <p>&nbsp;</p>
-                            <a href="http://capethemes.com/demo/processing" class="su-button su-button-style-flat bold" style="color:#343434;background-color:#fec24b;border-color:#cb9b3c;border-radius:2px;-moz-border-radius:2px;-webkit-border-radius:2px" target="_self"><span style="color:#343434;padding:6px 16px;font-size:13px;line-height:20px;border-color:#fed481;border-radius:2px;-moz-border-radius:2px;-webkit-border-radius:2px;text-shadow:none;-moz-text-shadow:none;-webkit-text-shadow:none"><i class="fa fa-clock-o" style="font-size:13px;color:#414141"></i> Just in Time</span></a>
-                            <a href="http://capethemes.com/demo/processing/contact-us/" class="su-button su-button-style-flat bold" style="color:#535353;background-color:#EFEFEF;border-color:#bfbfbf;border-radius:2px;-moz-border-radius:2px;-webkit-border-radius:2px" target="_self"><span style="color:#535353;padding:6px 16px;font-size:13px;line-height:20px;border-color:#f4f4f4;border-radius:2px;-moz-border-radius:2px;-webkit-border-radius:2px;text-shadow:none;-moz-text-shadow:none;-webkit-text-shadow:none"><i class="fa fa-desktop" style="font-size:13px;color:#414141"></i> Contact</span></a>
-
+                            <a href="<?php echo $url_1[0]; ?>" class="su-button su-button-style-flat bold" style="color:#343434;background-color:#fec24b;border-color:#cb9b3c;border-radius:2px;-moz-border-radius:2px;-webkit-border-radius:2px" target="_self"><span style="color:#343434;padding:6px 16px;font-size:13px;line-height:20px;border-color:#fed481;border-radius:2px;-moz-border-radius:2px;-webkit-border-radius:2px;text-shadow:none;-moz-text-shadow:none;-webkit-text-shadow:none"><i class="fa fa-clock-o" style="font-size:13px;color:#414141"></i> <?php echo $url_1_name[0];?> </span></a>
+                            <a href="<?php echo $url_2[0]; ?>" class="su-button su-button-style-flat bold" style="color:#535353;background-color:#EFEFEF;border-color:#bfbfbf;border-radius:2px;-moz-border-radius:2px;-webkit-border-radius:2px" target="_self"><span style="color:#535353;padding:6px 16px;font-size:13px;line-height:20px;border-color:#f4f4f4;border-radius:2px;-moz-border-radius:2px;-webkit-border-radius:2px;text-shadow:none;-moz-text-shadow:none;-webkit-text-shadow:none"><i class="fa fa-desktop" style="font-size:13px;color:#414141"></i> <?php echo $url_2_name[0];?></span></a>
                         </div>
-
                     </div>
-
-                </li>
-
-
-                <li>
-
-
-                    <a href="http://capethemes.com/demo/industry/templates/lightweight-material-engineering/" title="Heavy industry materials for machines" >
-
-                        <img width="1600" height="620" src="http://capethemes.com/demo/processing/wp-content/uploads/2015/07/steel-1968194-1600x620.jpg" class="tranz wp-post-image" alt="" />
-                    </a>
-
-
-                    <div class="flexinside rad tranz content_Left">
-
-                        <h2><a href="http://capethemes.com/demo/industry/templates/lightweight-material-engineering/" title="Heavy industry materials for machines" >Heavy industry materials for machines</a></h2>
-
-                        <div class="flexinside-inn">
-
-                            <p>Processing Inc. is responsible for  development of materials to advance heavy industry. Our expertise lies in understanding the properties and behaviours of substances.</p>
-                            <p>&nbsp;</p>
-                            <a href="http://capethemes.com/demo/processing/request-a-quote/" class="su-button su-button-style-flat bold" style="color:#535353;background-color:#BFDAE3;border-color:#99aeb6;border-radius:2px;-moz-border-radius:2px;-webkit-border-radius:2px" target="_self"><span style="color:#535353;padding:6px 16px;font-size:13px;line-height:20px;border-color:#d2e5eb;border-radius:2px;-moz-border-radius:2px;-webkit-border-radius:2px;text-shadow:none;-moz-text-shadow:none;-webkit-text-shadow:none"><i class="fa fa-desktop" style="font-size:13px;color:#414141"></i> Reqest more</span></a>
-
-                        </div>
-
-                    </div>
-
-                </li>
-
-
-
-
-            </ul>
-
-        </div>
+                <?php endif;?>
+            </li>
+        <?php endwhile; ?>
+    </ul>
+</div>
+<?php wp_reset_query();?>
